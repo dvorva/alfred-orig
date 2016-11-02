9eb0216e6190c381fe77a117da1a23e094f5f933
Davis Vorva <dvorva@0587341701.wireless.umich.net>
2016-11-01 20:31:57 -0400

    fixed log message

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def send_message(recipient_id, message_text):
 
 def send_picture_message(recipient_id):
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text="picture message."))
 
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
