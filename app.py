48a8da79870ecb298bec85ad4e6111d058841c4a
Davis Vorva <dvorva@0587341701.wireless.umich.net>
2016-11-01 21:10:36 -0400

    check if text comp

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ def webhook():
                     if messaging_event.get("message"):  # someone sent us a message
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                        message_text = messaging_event["message"]["text"]  # the message's text
-
-                        response = get_response(message_text, sender_id)
-                        send_message(sender_id, response)
+                        if messaging_event["message"].get("text"):
+                            message_text = messaging_event["message"]["text"]  # the message's text
+                            response = get_response(message_text, sender_id)
+                            send_message(sender_id, response)
 
                     if messaging_event.get("delivery"):  # delivery confirmation
                         pass
