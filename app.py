4c2141905f87799d89f7a9f3ab523b9d82d645d1
Davis Vorva <dvorva@0587383242.wireless.umich.net>
2016-11-04 18:00:35 -0400

    fix logger

--- app.py
+++ app.py
@@ -73,9 +73,9 @@ def oauth():
 def get_response(input_command, sender_id):
 	input_command = input_command.lower()
 
-	with open("~/logs/chat_messages.txt", "a") as requests_file:
-    	requests_file.write('{"class": X, "commandText": "' + input_command + '"},')
-
+	requests_file = open("~/logs/chat_messages.txt", "a")
+	requests_file.write('{"class": X, "commandText": "'+input_command + '"},')
+	requests_file.close()
 
 	if(input_command == "turn light on"):
 		return "I've turned your lights on."
