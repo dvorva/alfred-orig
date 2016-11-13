de6de4dc57416e5cc6226329c712a6fc5e8dbdfa
Davis Vorva <dvorva@daviss-air-2.gateway.pace.com>
2016-11-12 20:41:20 -0500

    what is that targeting call??

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def get_response(input_command, sender_id):
 			return "Your door is closed."
 		elif str(json_response[0]['value']) == "open":
 			return "Your door is open."
-		return "I should never print this."
+		return "Error, incorrect contact device response."
 	elif(classification_code == 9):
 		son_response = handle_smartthings_request_get("light/30")
 		return "I've done something to color light."
