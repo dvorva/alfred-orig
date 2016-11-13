a30f342a1459f54b116465d4586f68f9d7432bda
Davis Vorva <dvorva@daviss-air-2.gateway.pace.com>
2016-11-12 20:51:46 -0500

    new endpoint uri

--- app.py
+++ app.py
@@ -89,14 +89,14 @@ def oauth():
 def handle_smartthings_request_get(endpoint):
 	#GET -H "Authorization: Bearer ACCESS-TOKEN" "https://graph.api.smartthings.com/api/smartapps/endpoints"
 	authorization = "Bearer 4285e326-bb70-47b5-bf2b-02c3462609ae"
-	url = "https://graph-na02-useast1.api.smartthings.com:443/api/smartapps/installations/0e5e4208-87a9-48b7-8529-3b0e334dc073/" + endpoint
+	url = "https://graph-na02-useast1.api.smartthings.com:443/api/smartapps/installations/04193def-b6a8-415b-ae1a-24a51dfde0af/" + endpoint
 	r=requests.get(url, headers={"Authorization":authorization})
 	json_data = json.loads(r.text)
 	return json_data
 
 def handle_smartthings_request_put(endpoint):
 	authorization = "Bearer 4285e326-bb70-47b5-bf2b-02c3462609ae"
-	url = "https://graph-na02-useast1.api.smartthings.com:443/api/smartapps/installations/0e5e4208-87a9-48b7-8529-3b0e334dc073/" + endpoint
+	url = "https://graph-na02-useast1.api.smartthings.com:443/api/smartapps/installations/04193def-b6a8-415b-ae1a-24a51dfde0af/" + endpoint
 	r=requests.put(url, headers={"Authorization":authorization})
 	#log(url)
 
